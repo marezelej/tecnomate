773e85592d263af797d2edde438d3decb5b453e0
francozanini <31112262+francozanini@users.noreply.github.com>
2017-08-30 10:59:02 -0300

    Create tp7_ej6.cpp

---
+++ tp7_ej6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	char a[19];
+	char b[19];
+	 
+	for(int i = 0; i<20; i++){
+		cin >> a[i];
+    b[i] = a[i];
+		cout << b[i];
+	}
+	
+	return 0;
+}
+
