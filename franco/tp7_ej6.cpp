855698fa0b284c02d24d159d09169ba6e50056dd
marezelej <30838853+marezelej@users.noreply.github.com>
2017-08-30 11:08:37 -0300

    Arreglos

--- franco/tp7_ej6.cpp
+++ franco/tp7_ej6.cpp
@@ -3,15 +3,20 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	char a[19];
-	char b[19];
+	char a[10]; //Son los elementos del array, no el índice
+	char b[10];
 	 
-	for(int i = 0; i<20; i++){
+	for(int i = 0; i<10; i++){
 		cin >> a[i];
-    b[i] = a[i];
-		cout << b[i];
 	}
 	
+	for(int i = 0; i<10; i++){
+		b[i] = a[i];
+	}
+	
+	for(int i = 0; i<10; i++){
+		cout << b[i];
+	}
 	return 0;
 }
 
