69f9d06dd073aa306c8a198233217b000970ccf1
francozanini <31112262+francozanini@users.noreply.github.com>
2017-08-30 11:04:36 -0300

    Update tp7_ej6.cpp

--- franco/tp7_ej6.cpp
+++ franco/tp7_ej6.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	char a[19];
-	char b[19];
+	char a[20];
+	char b[20];
 	 
-	for(int i = 0; i<20; i++){
+	for(int i = 0; i<21; i++){
 		cin >> a[i];
-    b[i] = a[i];
+    		b[i] = a[i];
 		cout << b[i];
 	}
 	
