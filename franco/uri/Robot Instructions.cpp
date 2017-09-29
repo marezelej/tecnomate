ab447526c5eb25394908198d6482b0eb169dbf4a
francozanini <31112262+francozanini@users.noreply.github.com>
2017-09-28 22:43:31 -0300

    Update Robot Instructions.cpp

--- franco/uri/Robot Instructions.cpp
+++ franco/uri/Robot Instructions.cpp
@@ -1,5 +1,3 @@
-// https://www.urionlinejudge.com.br/judge/en/problems/view/1574
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,35 +9,33 @@ int main(int argc, char *argv[])
 	vector <int> v;
 	string instrucc;
 	cin >> casos; 
-	
+	int idem;
 	for(int i=0; i<casos; i++) 
 	{
 		cin>>mov;
 		int pos = 0;
-		int idem;
-		for(int j=0;j<mov;j++) 
+		for(int j=0;j<=mov;j++) 
 		{
-			cin>>instrucc;
-			if(instrucc == "RIGHT")
+			getline(cin,instrucc);
+			if(instrucc.size() == 5)
 			{
-				pos = pos +1;
+				pos = pos + 1;
 				v.push_back(1);	
 			}
-			else if(instrucc == "LEFT")
+			else if(instrucc.size() == 4)
 			{	
 			    pos = pos -1;
 				v.push_back(-1);
 			}
-			else if(instrucc == "SAME AS")
+			else if(instrucc.size() == 9)
 			{
-			    /* string toint */
-				idem = instrucc[6]
-				cout<<v[idem];
-				pos = pos + v[idem];
-			    v.push_back(v[idem]);
+				idem = instrucc[8] - '0';
+				pos = pos + v[idem-1];
+			    v.push_back(v[idem-1]);
 			}
 		}
 		cout<<pos<<endl;
+		v.erase(v.begin(),v.end());
 	}
 	return 0;
 }
