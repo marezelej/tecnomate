1c712bbcc5c8d12852a0ab21633c314e5397449a
francozanini <31112262+francozanini@users.noreply.github.com>
2017-10-07 18:02:47 -0300

    Create strings.cpp

---
+++ templates/strings.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+vector <string> pasaje(string & s, vector <string> & v)
+{
+	string limite = " ";
+	size_t pos = 0;
+	string sub;
+	while(((pos = s.find(limite)) != std::string::npos))
+	{
+		sub = s.substr(0, pos);
+		v.push_back(sub);
+		s.erase(0, pos + limite.length()); //porque find() cuenta desde 1
+	}
+	v.push_back(s); //la ultima palabra queda dentro de s porque no tiene espacio al final
+	s.clear();
+	return v;
+}
